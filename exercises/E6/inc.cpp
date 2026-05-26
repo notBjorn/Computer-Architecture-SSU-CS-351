34f325d68817821fcaf7e6115481aca5e68560ef
notBjorn Arch <228869939+notBjorn@users.noreply.github.com>
2026-05-26 01:22:16 -0700

    completed excercise 6

---
+++ exercises/E6/inc.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main()
+{
+    int i = 0;
+
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+    ++i;
+
+    printf("%d\n", i);
+
+    return EXIT_SUCCESS;
+}
